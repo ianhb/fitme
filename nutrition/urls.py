57f70de625a51d9286f5a69f21251725ca969669
Ian Blakley <ihb9@cornell.edu>
2016-03-19 13:37:15 -0400

    Added Base Page for nutrition

--- nutrition/urls.py
+++ nutrition/urls.py
@@ -1,7 +1,22 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from nutrition import views
 
+log_urls = [
+    url(r'^new/$', views.log_foods, name='log_food'),
+    url(r'^day/', views.day_log, name='day_log'),
+    url(r'^week/', views.week_log, name='week_log'),
+    url(r'^month/', views.month_log, name='month_log')
+]
+
+goals_urls = [
+    url(r'^view/', views.view_goals, name='view_goals'),
+    url(r'^set/', views.set_goals, name='set_goals')
+]
+
 urlpatterns = [
     url(r'^$', views.nutrition_home, name='nutrition_home'),
+    url(r'^search/$', views.search_foods, name='search_foods'),
+    url(r'^log/', include(log_urls)),
+    url(r'^goals/', include(goals_urls))
 ]
--- nutrition/views.py
+++ nutrition/views.py
@@ -3,4 +3,39 @@ from django.shortcuts import render
 
 
 def nutrition_home(request):
-    return render(request, 'base/base.html')
+    return render(request, 'nutrition/base.html')
+
+
+def search_foods(request):
+    # TODO
+    return render(request, 'nutrition/base.html')
+
+
+def log_foods(request):
+    # TODO
+    return render(request, 'nutrition/base.html')
+
+
+def day_log(request):
+    # TODO
+    return render(request, 'nutrition/base.html')
+
+
+def week_log(request):
+    # TODO
+    return render(request, 'nutrition/base.html')
+
+
+def month_log(request):
+    # TODO
+    return render(request, 'nutrition/base.html')
+
+
+def view_goals(request):
+    # TODO
+    return render(request, 'nutrition/base.html')
+
+
+def set_goals(request):
+    # TODO
+    return render(request, 'nutrition/base.html')
